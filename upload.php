ea019c9a7342f3be7ec1e8f904feaf4bb0fd571c
jimlao <jimlaogithub@gmail.com>
2020-03-05 05:11:30 +0800

    master

--- upload.php
+++ upload.php
@@ -1,23 +1,45 @@
 <?php
     include_once('validation.php'); 
     $files = $_FILES['files'];
+    $dir = "uploads/";
 
     
     foreach($files['name'] as $position => $fileName){
         //validating each file
-        if(checkType($files['types'][$position])){
+        //echo $files['type'][$position];
+        if(checkType($files['type'][$position])){
             if(checkSize($files['size'][$position])){
-
+                $targetDir = $dir.basename($files['name'][$position]);
+                if(checkIfFileExist($targetDir)){
+                   if(moveFile($files['tmp_name'][$position],$targetDir))
+                    echo "[{$files['name'][$position]}] succesful <br>";
+                }else{
+                    echo "[{$files['name'][$position]}] file exist <br>";
+                }
             }else{
                 echo "[{$files['name'][$position]}] file To Large <br>";
             }
         }else{
-            echo "[{$files['names'][$position]}] this file type is not allowed <br>";
+            echo "[{$files['name'][$position]}] this file type is not allowed <br>";
         }
     }
 
     
+    
 
+    //scan "uploads" folder and display them accordingly
+    $folder = "uploads";
+    $results = scandir('uploads');
+    foreach ($results as $result) {
+        if ($result === '.' or $result === '..') continue;
+
+        if (is_file($folder . '/' . $result)) {
+            echo '
+                <div class="col-md-3">
+                    <img src="'.$folder . '/' . $result.'" alt="..." style="width:250px; height:auto;">
+                </div>';
+        }
+    }
     
 
 
--- validation.php
+++ validation.php
@@ -13,4 +13,11 @@
         return ($size <= 1000000) ? true : false;
     }
 
+    function checkIfFileExist($file){
+        return (!file_exists($file)) ? true : false;
+    }
+
+    function moveFile($file,$targetDir){
+        return  move_uploaded_file($file,$targetDir);
+    }
 ?>
